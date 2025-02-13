82340bb8383373ccbea875a494db96c55086f744
Keijiro Takahashi <keijiro@gmail.com>
2025-02-13 16:48:11 +0900

    Properties

--- Packages/jp.keijiro.kino.feedback.universal/Runtime/FeedbackEffect.cs
+++ Packages/jp.keijiro.kino.feedback.universal/Runtime/FeedbackEffect.cs
@@ -8,6 +8,25 @@ namespace Kino.Feedback.Universal {
 [AddComponentMenu("Kino/Feedback Effect")]
 public sealed class FeedbackEffect : MonoBehaviour
 {
+    #region Public properties (serialized)
+
+    [field:SerializeField, ColorUsage(false)]
+    public Color Tint { get; set; } = Color.white;
+
+    [field:SerializeField]
+    public float HueShift { get; set; }
+
+    [field:SerializeField]
+    public Vector2 Offset { get; set; }
+
+    [field:SerializeField]
+    public float Rotation { get; set; }
+
+    [field:SerializeField]
+    public float Scale { get; set; } = 1.1f;
+
+    #endregion
+
     #region Public members exposed for render passes
 
     public bool IsReady => Properties != null;
@@ -29,6 +48,12 @@ public sealed class FeedbackEffect : MonoBehaviour
 
     RTHandle _buffer;
 
+    Vector4 GetTransformVector()
+    {
+        var r = Mathf.Deg2Rad * Rotation;
+        return new Vector4(Mathf.Sin(r), Mathf.Cos(r), Offset.x, Offset.y) / Scale;
+    }
+
     #endregion
 
     #region MonoBehaviour implementation
@@ -39,9 +64,12 @@ public sealed class FeedbackEffect : MonoBehaviour
     void LateUpdate()
     {
         if (Properties == null) Properties = new MaterialPropertyBlock();
-        Properties.SetFloat("_Test", 1);
-        if (_buffer != null)
-            Properties.SetTexture("_FeedbackTexture", _buffer);
+
+        if (_buffer != null) Properties.SetTexture("_FeedbackTexture", _buffer);
+
+        Properties.SetVector("_Transform", GetTransformVector());
+        Properties.SetColor("_Tint", Tint);
+        Properties.SetFloat("_HueShift", HueShift);
     }
 
     #endregion
--- Packages/jp.keijiro.kino.feedback.universal/Shaders/Feedback.shader
+++ Packages/jp.keijiro.kino.feedback.universal/Shaders/Feedback.shader
@@ -7,6 +7,26 @@ HLSLINCLUDE
 
 TEXTURE2D(_FeedbackTexture);
 
+float4 _Transform;
+float4 _Tint;
+float _HueShift;
+
+float3x3 ConstructTransformMatrix()
+{
+    return float3x3(_Transform.y, -_Transform.x, _Transform.z,
+                    _Transform.x,  _Transform.y, _Transform.w,
+                               0,             0,            1);
+}
+
+float3 ApplyTint(float3 rgb)
+{
+    rgb = RgbToHsv(rgb);
+    rgb.x = frac(rgb.x + _HueShift);
+    rgb = HsvToRgb(rgb);
+    rgb *= _Tint.rgb;
+    return rgb;
+}
+
 void VertInjection(uint vertexID : VERTEXID_SEMANTIC,
                    out float4 outPosition : SV_Position,
                    out float2 outTexCoord : TEXCOORD0)
@@ -20,8 +40,17 @@ float4 FragInjection(float4 position : SV_Position,
                      float2 texCoord : TEXCOORD) : SV_Target0
 {
     float2 uv = texCoord;
-    uv = (uv - 0.5) * 0.9 + 0.5;
-    return SAMPLE_TEXTURE2D(_FeedbackTexture, sampler_LinearClamp, uv);
+
+    // Feedback transform
+    uv = mul(ConstructTransformMatrix(), float3(uv - 0.5, 1)).xy + 0.5;
+
+    // Feedback sample
+    float4 c = SAMPLE_TEXTURE2D(_FeedbackTexture, sampler_LinearClamp, uv);
+
+    // Composition
+    c.rgb = ApplyTint(c.rgb);
+
+    return c;
 }
 
 ENDHLSL
