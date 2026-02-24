0d1d74afc808ce465b6c1ef3376e5fa4fa39c79e
Keijiro Takahashi <keijiro@gmail.com>
2026-02-24 21:38:37 +0900

    Simplify editor code

--- Packages/jp.keijiro.kino.feedback.universal/Editor/FeedbackControllerInspector.cs
+++ Packages/jp.keijiro.kino.feedback.universal/Editor/FeedbackControllerInspector.cs
@@ -7,14 +7,8 @@ namespace Kino.Feedback.Universal {
 [CustomEditor(typeof(FeedbackController))]
 public sealed class FeedbackControllerInspector : Editor
 {
-    public VisualTreeAsset _xml;
-
-    public override VisualElement CreateInspectorGUI()
-    {
-        _xml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>
-          ("Packages/jp.keijiro.kino.feedback.universal/Editor/FeedbackController.uxml");
-        return _xml.Instantiate();
-    }
+    public VisualTreeAsset _uxml;
+    public override VisualElement CreateInspectorGUI() => _uxml.CloneTree();
 }
 
 } // namespace Kino.Feedback.Universal
